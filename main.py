5ddf49196604ee4f40de8a232146d81758bdeb40
GGURIMON <ehdrhks105@naver.com>
2024-10-15 21:43:08 +0900

    tags and summary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 from enum import Enum
 from typing import Optional
 
@@ -8,19 +8,19 @@ app = FastAPI()
 def index():
     return {"message":"Hello World"}
 
-@app.get("/blog/me")
+@app.get("/blog/me", tags=['blog'])
 def get_blog_me():
     return {"message": "my blog"}
 
-@app.get('/blog/q')
+@app.get('/blog/q', tags=['blog', 'query'])
 def get_blog_search(page, page_size: Optional[int] = None):
     return {"message": f"you can get your blog on {page} and your page size is {page_size}"}
 
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}',tags=['blog'])
 def user_get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {"my message": f"blog_id: {id}, comment id: {comment_id}, valid: {valid}, username: {username}"}
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', tags=['blog'])
 def get_blog(id):
     return {"message": f"blog with {id}"}
 
@@ -31,8 +31,15 @@ class BlogType(str, Enum):
     howto = 'howto'
 
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def get_blog_type(type: BlogType):
     return {"message": f"Blog type: {type}"}
 
-
+@app.get('/naver/search/{query}', status_code = status.HTTP_200_OK, tags=['query'], summary= "status code testing", description= "status code with temp situation for learning")
+def get_search_naver(query: int, response: Response):
+    if query > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": f"{query} page not found!"}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {"message": f"here is your {query} page!"}
