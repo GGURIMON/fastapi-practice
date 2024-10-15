ca43a9fe0d9835ea6e42b19432c054c40faa3162
GGURIMON <ehdrhks105@naver.com>
2024-10-15 20:38:03 +0900

    get method fastapi

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message":"Hello World"}
+
+@app.get("/blog/me")
+def get_blog_me():
+    return {"message": "my blog"}
+
+@app.get('/blog/{id}')
+def get_blog(id):
+    return {"message": f"blog with {id}"}
+
+# Enum 정의 수정 (콜론 대신 = 사용)
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return {"message": f"Blog type: {type}"}
