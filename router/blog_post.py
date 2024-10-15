da47d771c4a432ef3798c91966a22f0799255da5
GGURIMON <ehdrhks105@naver.com>
2024-10-16 00:04:48 +0900

    tried to figure it out Query param

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Request
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 router = APIRouter(
     prefix = "/blog",
@@ -10,6 +10,14 @@ class BlogModel(BaseModel):
     content: str
     imageMap: list
 
-@router.post('/new')
-def create_blog(blog: BaseModel):
-    return "Ok"
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        "id": id,
+        "data": blog,
+        "version": version
+    }
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int, comment_id: int = Query(None, title="id of the comment", description="Some description for comment_id", alias="commentId")):
+    pass
